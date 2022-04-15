ae1d098ee7c839dddeb0acc73a32cce5c156ed6a
GitterhipOo <1034902175@qq.com>
2022-04-15 15:53:24 +0800

    优化适配性

--- scnusay.cc/about/about.js
+++ scnusay.cc/about/about.js
@@ -1,13 +1,6 @@
 window.onload = function () {
     var setwidth = document.getElementById("wrapper");
-    var maxpx = document.body.clientWidth;
-    console.log(maxpx);
-    setwidth.style.width = maxpx + 'px';
-
-}
-window.onresize = function () {
-    var setwidth = document.getElementById("wrapper");
-    var maxpx = document.body.clientWidth;
+    var maxpx = screen.width;
     console.log(maxpx);
     setwidth.style.width = maxpx + 'px';
 }
--- scnusay.cc/us/us.js
+++ scnusay.cc/us/us.js
@@ -1,12 +1,7 @@
 window.onload = function () {
     var setwidth = document.getElementById("wrapper");
-    var maxpx = document.body.clientWidth;
+    var maxpx = screen.width;
     console.log(maxpx);
     setwidth.style.width = maxpx + 'px';
-}
-window.onresize = function () {
-    var setwidth = document.getElementById("wrapper");
-    var maxpx = document.body.clientWidth;
-    console.log(maxpx);
-    setwidth.style.width = maxpx + 'px';
+    document.getElementById("home").style.width = maxpx * 0.7 + 'px';
 }
