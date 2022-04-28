b19e9d85fa0851d17035a63653df8e8c485815ff
GitterhipOo <1034902175@qq.com>
2022-04-28 14:40:34 +0800

    编写注册数据提交

--- miniprogram-1/pages/signup/index.js
+++ miniprogram-1/pages/signup/index.js
@@ -1,72 +1,115 @@
 // pages/signup/index.js
+const app=getApp();
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        username:"",
+        password:"",
+        passwordAck:"",
+        userphone:"",
+        scnu_number:""
     },
     enroll: function (e) {
         // 发起网络请求
-        wx.navigateTo({
-        // 开发者服务器接口地址
-          url: '/pages/index/login',
+        wx.navigateBack({
+            // 返回上 1 页
+            delta: 1
         })
       },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+      regist:function(e){
+          console.log(e);
+        var that = this;
+        if(that.data.username == ''){
+          wx.showModal({
+            title: '提示',
+            content: '请输入用户名',
+            showCancel:false,
+            success (res) {
+            }
+          })
+        }else if(that.data.password == ''){
+          wx.showModal({
+            title: '提示',
+            content: '请输入密码',
+            showCancel:false,
+            success (res) {
+            }
+          })
+        }else if(that.data.passwordAck == ''){
+          wx.showModal({
+            title: '提示',
+            content: '请再次输入密码',
+            showCancel:false,
+            success (res) {
+            }
+          })
+        }else if(that.data.passwordAck != that.data.password){
+          wx.showModal({
+            title: '提示',
+            content: '两次密码输入不一致',
+            showCancel:false,
+            success (res) {
+            }
+          })
+        }else if(that.data.userphone== ''){
+          wx.showModal({
+            title: '提示',
+            content: '请输入手机号码',
+            showCancel:false,
+            success (res) {
+            }
+          })
+        }else if(that.data.scnu_number == ''){
+          wx.showModal({
+            title: '提示',
+            content: '请输入学号',
+            showCancel:false,
+            success (res) {
+            }
+          })
+        }else if(that.data.userphone.length != 11){
+          wx.showModal({
+            title: '提示',
+            content: '手机号位数不正确，请重新输入',
+            showCancel:false,
+            success (res) {
+            }
+          })
+        }else if(that.data.scnu_number.length != 11){
+          wx.showModal({
+            title: '提示',
+            content: '学号不合法',
+            showCancel:false,
+            success (res) {
+            }
+          })
+        }
+      },
 
-    },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+      usernameInput:function(e){
+        // 获取 input 输入框的值
+        this.data.username = e.detail.value;
+      },
+      passwordInput:function(e){
+        // 获取 input 输入框的值
+        this.data.password = e.detail.value;
+      },
+      passwordInputAck:function(e){
+        // 获取 input 输入框的值
+        this.data.passwordAck = e.detail.value;
+      },
+      emailInput:function(e){
+        // 获取 input 输入框的值
+        this.data.userphone = e.detail.value;
+      },
+      phoneNumberInput:function(e){
+        // 获取 input 输入框的值
+        this.data.scnu_number = e.detail.value;
+      },
 
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+ 
 })
