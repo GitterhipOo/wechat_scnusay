bd89187c9089238d29176430a429766c3f8ca6ab
Paker-kk <921693422@qq.com>
2022-08-27 21:16:05 +0800

    kf的

--- wechat_scnusay-main/www.scnusay.cc/index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var setwidth = document.getElementById("wrapper");
-    var maxpx = screen.width;
-    console.log(maxpx);
-    setwidth.style.width = maxpx + 'px';
-    document.getElementById("whithback").style.width = maxpx * 0.7 + 'px';
-    document.getElementById("indeximg").style.width = maxpx * 0.7 + 'px';
-    document.getElementById("wrapper2").style.width = maxpx + 'px';
-}
