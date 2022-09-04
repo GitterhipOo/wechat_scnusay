7c357d4804e38c8eca8012b14a2cbd3204423410
akai34 <88494753+akai34@users.noreply.github.com>
2022-09-04 22:17:44 +0800

    1

--- miniprogram-1/pages/lostthing/index.js
+++ miniprogram-1/pages/lostthing/index.js
@@ -8,167 +8,171 @@ Page({
      * 页面的初始数据
      */
     data: {
-      top_using_res:wx.getMenuButtonBoundingClientRect(),
-      windowInfo: wx.getWindowInfo(),
-      sortlist: [
-          {
-              sortid: 1,
-              icon: "../../assets/images/sort/all.png",
-              text: "全部",
-              jump: "all",
-              chosen_judge:"#fff"
-          },
-          {
-              sortid: 2,
-              icon: "../../assets/images/sort/book.png",
-              text: "图书文具",
-              jump: "book",
-              chosen_judge:"#fff"
-          },
-          {
-              sortid: 3,
-              icon: "../../assets/images/sort/life.png",
-              text: "生活用品",
-              jump: "life",
-              chosen_judge:"#fff"
-          },
-          {
-              sortid: 4,
-              icon: "../../assets/images/sort/computer.png",
-              text: "电子产品",
-              jump: "computer",
-              chosen_judge:"#fff"
-          },
-          {
-              sortid: 5,
-              icon: "../../assets/images/sort/makeup.png",
-              text: "化妆用品",
-              jump: "makeup",
-              chosen_judge:"#fff"
-          },
-          {
-              sortid: 6,
-              icon: "../../assets/images/sort/clothe.png",
-              text: "服饰鞋包",
-              jump: "clothe",
-              chosen_judge:"#fff"
-          },
-          {
-              sortid: 7,
-              icon: "../../assets/images/sort/others.png",
-              text: "其他",
-              jump: "others",
-              chosen_judge:"#fff"
-          },
-          {
-              sortid: 8,
-              icon: "../../assets/images/sort/publish.png",
-              text: "我的发布",
-              jump: "mypublish",
-              chosen_judge:"#fff"
-          }
-      ],
+        top_using_res: wx.getMenuButtonBoundingClientRect(),
+        windowInfo: wx.getWindowInfo(),
+        sortlist: [{
+                sortid: 1,
+                icon: "../../assets/images/sort/all.png",
+                text: "全部",
+                jump: "all",
+                chosen_judge: "#fff"
+            },
+            {
+                sortid: 2,
+                icon: "../../assets/images/sort/book.png",
+                text: "图书文具",
+                jump: "book",
+                chosen_judge: "#fff"
+            },
+            {
+                sortid: 3,
+                icon: "../../assets/images/sort/life.png",
+                text: "生活用品",
+                jump: "life",
+                chosen_judge: "#fff"
+            },
+            {
+                sortid: 4,
+                icon: "../../assets/images/sort/computer.png",
+                text: "电子产品",
+                jump: "computer",
+                chosen_judge: "#fff"
+            },
+            {
+                sortid: 5,
+                icon: "../../assets/images/sort/makeup.png",
+                text: "化妆用品",
+                jump: "makeup",
+                chosen_judge: "#fff"
+            },
+            {
+                sortid: 6,
+                icon: "../../assets/images/sort/clothe.png",
+                text: "服饰鞋包",
+                jump: "clothe",
+                chosen_judge: "#fff"
+            },
+            {
+                sortid: 7,
+                icon: "../../assets/images/sort/others.png",
+                text: "其他",
+                jump: "others",
+                chosen_judge: "#fff"
+            },
+            {
+                sortid: 8,
+                icon: "../../assets/images/sort/publish.png",
+                text: "我的发布",
+                jump: "mypublish",
+                chosen_judge: "#fff"
+            }
+        ],
         navH: 0,
-        tags:[{
-          "src":"/assets/images/sort/news.png",
-          "tag_Desc":"文具",
-          "id":"1",
-        },{
-          "src":"/assets/images/sort/news.png",
-          "tag_Desc":"文具",
-          "id":"2",
-        },{
-          "src":"/assets/images/sort/news.png",
-          "tag_Desc":"文具",
-          "id":"3",
-        },{
-          "src":"/assets/images/sort/news.png",
-          "tag_Desc":"文具",
-          "id":"4",
-        },{
-          "src":"/assets/images/sort/news.png",
-          "tag_Desc":"文具",
-          "id":"5",
-        },{
-          "src":"/assets/images/sort/news.png",
-          "tag_Desc":"文具",
-          "id":"6",
-        },{
-          "src":"/assets/images/sort/news.png",
-          "tag_Desc":"文具",
-          "id":"7",
-        },{
-          "src":"/assets/images/sort/news.png",
-          "tag_Desc":"文具",
-          "id":"8",
-        },],
+        tags: [{
+            "src": "/assets/images/sort/news.png",
+            "tag_Desc": "文具",
+            "id": "1",
+        }, {
+            "src": "/assets/images/sort/news.png",
+            "tag_Desc": "文具",
+            "id": "2",
+        }, {
+            "src": "/assets/images/sort/news.png",
+            "tag_Desc": "文具",
+            "id": "3",
+        }, {
+            "src": "/assets/images/sort/news.png",
+            "tag_Desc": "文具",
+            "id": "4",
+        }, {
+            "src": "/assets/images/sort/news.png",
+            "tag_Desc": "文具",
+            "id": "5",
+        }, {
+            "src": "/assets/images/sort/news.png",
+            "tag_Desc": "文具",
+            "id": "6",
+        }, {
+            "src": "/assets/images/sort/news.png",
+            "tag_Desc": "文具",
+            "id": "7",
+        }, {
+            "src": "/assets/images/sort/news.png",
+            "tag_Desc": "文具",
+            "id": "8",
+        }, ],
         post: [{
-            blogger_id: 1, //文章所属id
-            blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
-            blogger_name:"xhiming",//博主昵称
-            blogger_time: "1661857644662", //发布时间的时间戳、这里需要修改
-            lostthing_topic: "700出帅哥一只", //标题
-            lostthing_time:"1661857644662",//丢失时间的时间戳、这里需要修改
-            lostthing_class: "失物求寻", //发布类别（不需要可以不填充
-            lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
-            lostthing_space:"南海校区",//
-            lostthing_space_detail:"G253与G252之间的交界处",
-            lostthing_contact:"12312311231",
-            photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
-            tags: ["图书文具", "生活用品", "夹心糖"], //标签
-            readingtimes: 49, //阅读次数
-            comments: 5, //评论数量
-            favour: 20, //点赞数量
-            had_favour: 0,//点赞判断
-            favour_src: "/assets/images/icon/unfavour.png",//点赞图标
-        },
-        {
-          blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
-          lostthing_topic: "70出帅哥一只", //标题
-          blogger_time: "0", //时间戳、这里需要
-          lostthing_class: "失物求寻", //发布类别（不需要可以不填充
-          lostthing_detail: "我在南海这里丢失抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
-          photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
-          tags: ["图书文具", "生活用品", "夹心糖"], //标签
-          readingtimes: 49, //阅读次数
-          favour: 20, //点赞数量
-          had_favour: 0,
-          favour_src: "/assets/images/icon/unfavour.png",
-          comments: 5, //评论数量
-          blogger_id: 1, //文章在数据库中存储的位置
-      }],
+                blogger_id: 1, //文章所属id
+                blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
+                blogger_name: "xhiming", //博主昵称
+                blogger_time: "1661857644662", //发布时间的时间戳、这里需要修改
+                lostthing_topic: "700出帅哥一只", //标题
+                lostthing_time: "1661857644662", //丢失时间的时间戳、这里需要修改
+                lostthing_class: "失物求寻", //发布类别（不需要可以不填充
+                lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
+                lostthing_space: "南海校区", //
+                lostthing_space_detail: "G253与G252之间的交界处",
+                lostthing_contact: "12312311231",
+                photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
+                tags: ["图书文具", "生活用品", "夹心糖"], //标签
+                readingtimes: 49, //阅读次数
+                comments: 5, //评论数量
+                favour: 20, //点赞数量
+                had_favour: 0, //点赞判断
+                favour_src: "/assets/images/icon/unfavour.png", //点赞图标
+            },
+            {
+                blogger_id: 2, //文章所属id
+                blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
+                blogger_name: "zhiming", //博主昵称
+                blogger_time: "1661857644662", //发布时间的时间戳、这里需要修改
+                lostthing_topic: "帅哥一只", //标题
+                lostthing_time: "1661857644662", //丢失时间的时间戳、这里需要修改
+                lostthing_class: "失物求寻", //发布类别（不需要可以不填充
+                lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
+                lostthing_space: "南海校区", //
+                lostthing_space_detail: "G253与G252之间的交界处",
+                lostthing_contact: "12312311231",
+                photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
+                tags: ["图书文具", "生活用品", "夹心糖"], //标签
+                readingtimes: 49, //阅读次数
+                comments: 5, //评论数量
+                favour: 20000, //点赞数量
+                had_favour: 0, //点赞判断
+                favour_src: "/assets/images/icon/unfavour.png", //点赞图标
+            },
+        ],
 
         //轮播图的照片
-        photos:["https://s1.328888.xyz/2022/08/29/CzMYU.png","https://s1.328888.xyz/2022/08/29/CzgoR.png","https://s1.328888.xyz/2022/08/29/Czf0B.png"],
+        photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"],
     },
-    jumpToSearch:function(){
-      wx.navigateTo({
-        url: '/pages/lostthing/search',
-      })
+    jumpToSearch: function () {
+        wx.navigateTo({
+            url: '/pages/lostthing/search',
+        })
     },
-    
-    jumptonews(){
-      wx.navigateTo({
-        url: '/pages/lostthing/details',
-      })
+
+    jumptonews() {
+        wx.navigateTo({
+            url: '/pages/lostthing/details',
+        })
     },
     //分类选择功能
     ///////根据选取的序号判断内容，并将选取的模块颜色设置为#eee
-    class_choose:function(e){
-      var chosennum=e.currentTarget.dataset.sortid-1
-      for(var i=0;i<8;i=i+1){
-        if(chosennum!==i){
-          console.log(this.data.sortlist[i].chosen_judge)
-          this.setData({
-            ['sortlist[' + i + '].chosen_judge']:"#fff",
-          })
+    class_choose: function (e) {
+        var chosennum = e.currentTarget.dataset.sortid - 1
+        for (var i = 0; i < 8; i = i + 1) {
+            if (chosennum !== i) {
+                console.log(this.data.sortlist[i].chosen_judge)
+                this.setData({
+                    ['sortlist[' + i + '].chosen_judge']: "#fff",
+                })
+            } else {
+                this.setData({
+                    ['sortlist[' + i + '].chosen_judge']: "#eee"
+                })
+            }
         }
-        else{
-          this.setData({
-            ['sortlist[' + i + '].chosen_judge']:"#eee"
-          })
-        }
-      }
     },
 
     //点赞功能
@@ -179,15 +183,15 @@ Page({
         if (this.data.post[blogger_id].had_favour == 0) {
             console.log(1)
             this.setData({
-                ['post['+blogger_id+'].favour']: that.data.post[blogger_id].favour + 1,
-                ['post['+blogger_id+'].favour_src']: "/assets/images/icon/favour.png",
-                ['post['+blogger_id+'].had_favour']: 1
+                ['post[' + blogger_id + '].favour']: that.data.post[blogger_id].favour + 1,
+                ['post[' + blogger_id + '].favour_src']: "/assets/images/icon/favour.png",
+                ['post[' + blogger_id + '].had_favour']: 1
             })
         } else {
             this.setData({
-                ['post['+blogger_id+'].favour']: that.data.post[blogger_id].favour - 1,
-                ['post['+blogger_id+'].favour_src']: "/assets/images/icon/unfavour.png",
-                ['post['+blogger_id+'].had_favour']: 0
+                ['post[' + blogger_id + '].favour']: that.data.post[blogger_id].favour - 1,
+                ['post[' + blogger_id + '].favour_src']: "/assets/images/icon/unfavour.png",
+                ['post[' + blogger_id + '].had_favour']: 0
             })
         }
     },
@@ -198,15 +202,15 @@ Page({
     onLoad: function (options) {
         this.setData({
             navH: app.globalData.navHeight
-          });
+        });
     },
     logo: function (e) {
         // 发起网络请求
         wx.navigateTo({
-        // 开发者服务器接口地址
-          url: '/pages/index/index',
+            // 开发者服务器接口地址
+            url: '/pages/index/index',
         })
-      },
+    },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
@@ -253,7 +257,6 @@ Page({
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
-    }
-    
+    onShareAppMessage: function () {}
+
 })
