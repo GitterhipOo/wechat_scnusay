92b6ac2274ff3f7f183f264ab3f1402645c64468
akai34 <88494753+akai34@users.noreply.github.com>
2022-09-22 13:20:46 +0800

    绑定按钮数据
    如题

--- miniprogram-1/pages/lostthing/index.js
+++ miniprogram-1/pages/lostthing/index.js
@@ -15,14 +15,15 @@ Page({
         navH: 0,
         height: '',
         heights: [],
-        post: [{
+        //post1为丢失物品，其中lostthing_class = 1
+        post1: [{
                 blogger_id: 1, //文章所属id
                 blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
                 blogger_name: "xhiming", //博主昵称
-                blogger_time: "1661857644662", //发布时间的时间戳、这里需要修改
-                lostthing_topic: "700出帅哥一只", //标题
-                lostthing_time: "1661857644662", //丢失时间的时间戳、这里需要修改
-                lostthing_class: "失物求寻", //发布类别（不需要可以不填充
+                blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
+                lostthing_topic: "700出2帅哥一只", //标题
+                lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
+                lostthing_class: "1", //发布类别（不需要可以不填充
                 lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
                 lostthing_space: "南海校区", //
                 lostthing_space_detail: "G253与G252之间的交界处",
@@ -39,10 +40,130 @@ Page({
                 blogger_id: 2, //文章所属id
                 blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
                 blogger_name: "zhiming", //博主昵称
-                blogger_time: "1661857644662", //发布时间的时间戳、这里需要修改
-                lostthing_topic: "帅哥一只", //标题
-                lostthing_time: "1661857644662", //丢失时间的时间戳、这里需要修改
-                lostthing_class: "失物求寻", //发布类别（不需要可以不填充
+                blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
+                lostthing_topic: "帅哥一22222", //标题
+                lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
+                lostthing_class: "1", //发布类别（不需要可以不填充
+                lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
+                lostthing_space: "南海校区", //
+                lostthing_space_detail: "G253与G252之间的交界处",
+                lostthing_contact: "12312311231",
+                photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
+                tags: ["图书文具", "生活用品", "夹心糖"], //标签
+                readingtimes: 49, //阅读次数
+                comments: 5, //评论数量
+                favour: 20000, //点赞数量
+                had_favour: 0, //点赞判断
+                favour_src: "/assets/images/icon/unfavour.png", //点赞图标
+            },
+            {
+                blogger_id: 2, //文章所属id
+                blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
+                blogger_name: "zhiming", //博主昵称
+                blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
+                lostthing_topic: "帅哥一22222", //标题
+                lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
+                lostthing_class: "1", //发布类别（不需要可以不填充
+                lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
+                lostthing_space: "南海校区", //
+                lostthing_space_detail: "G253与G252之间的交界处",
+                lostthing_contact: "12312311231",
+                photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
+                tags: ["图书文具", "生活用品", "夹心糖"], //标签
+                readingtimes: 49, //阅读次数
+                comments: 5, //评论数量
+                favour: 20000, //点赞数量
+                had_favour: 0, //点赞判断
+                favour_src: "/assets/images/icon/unfavour.png", //点赞图标
+            },
+            {
+                blogger_id: 2, //文章所属id
+                blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
+                blogger_name: "zhiming", //博主昵称
+                blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
+                lostthing_topic: "帅哥一22222", //标题
+                lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
+                lostthing_class: "1", //发布类别（不需要可以不填充
+                lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
+                lostthing_space: "南海校区", //
+                lostthing_space_detail: "G253与G252之间的交界处",
+                lostthing_contact: "12312311231",
+                photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
+                tags: ["图书文具", "生活用品", "夹心糖"], //标签
+                readingtimes: 49, //阅读次数
+                comments: 5, //评论数量
+                favour: 20000, //点赞数量
+                had_favour: 0, //点赞判断
+                favour_src: "/assets/images/icon/unfavour.png", //点赞图标
+            },
+            {
+                blogger_id: 2, //文章所属id
+                blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
+                blogger_name: "zhiming", //博主昵称
+                blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
+                lostthing_topic: "帅哥一22222", //标题
+                lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
+                lostthing_class: "1", //发布类别（不需要可以不填充
+                lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
+                lostthing_space: "南海校区", //
+                lostthing_space_detail: "G253与G252之间的交界处",
+                lostthing_contact: "12312311231",
+                photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
+                tags: ["图书文具", "生活用品", "夹心糖"], //标签
+                readingtimes: 49, //阅读次数
+                comments: 5, //评论数量
+                favour: 20000, //点赞数量
+                had_favour: 0, //点赞判断
+                favour_src: "/assets/images/icon/unfavour.png", //点赞图标
+            },
+            {
+                blogger_id: 2, //文章所属id
+                blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
+                blogger_name: "zhiming", //博主昵称
+                blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
+                lostthing_topic: "帅哥一22222", //标题
+                lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
+                lostthing_class: "1", //发布类别（不需要可以不填充
+                lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
+                lostthing_space: "南海校区", //
+                lostthing_space_detail: "G253与G252之间的交界处",
+                lostthing_contact: "12312311231",
+                photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
+                tags: ["图书文具", "生活用品", "夹心糖"], //标签
+                readingtimes: 49, //阅读次数
+                comments: 5, //评论数量
+                favour: 20000, //点赞数量
+                had_favour: 0, //点赞判断
+                favour_src: "/assets/images/icon/unfavour.png", //点赞图标
+            },
+            {
+                blogger_id: 2, //文章所属id
+                blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
+                blogger_name: "zhiming", //博主昵称
+                blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
+                lostthing_topic: "帅哥一22222", //标题
+                lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
+                lostthing_class: "1", //发布类别（不需要可以不填充
+                lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
+                lostthing_space: "南海校区", //
+                lostthing_space_detail: "G253与G252之间的交界处",
+                lostthing_contact: "12312311231",
+                photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
+                tags: ["图书文具", "生活用品", "夹心糖"], //标签
+                readingtimes: 49, //阅读次数
+                comments: 5, //评论数量
+                favour: 20000, //点赞数量
+                had_favour: 0, //点赞判断
+                favour_src: "/assets/images/icon/unfavour.png", //点赞图标
+            },
+            {
+                blogger_id: 2, //文章所属id
+                blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
+                blogger_name: "zhiming", //博主昵称
+                blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
+                lostthing_topic: "帅哥一22222", //标题
+                lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
+                lostthing_class: "1", //发布类别（不需要可以不填充
                 lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
                 lostthing_space: "南海校区", //
                 lostthing_space_detail: "G253与G252之间的交界处",
@@ -56,11 +177,50 @@ Page({
                 favour_src: "/assets/images/icon/unfavour.png", //点赞图标
             },
         ],
+        //post2为捡到物品，其中lostthing_class = 2
+        post2: [{
+            blogger_id: 1, //文章所属id
+            blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
+            blogger_name: "xhiming", //博主昵称
+            blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
+            lostthing_topic: "700出帅哥一只22222", //标题
+            lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
+            lostthing_class: "2", //发布类别（不需要可以不填充
+            lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
+            lostthing_space: "南海校区", //
+            lostthing_space_detail: "G253与G252之间的交界处",
+            lostthing_contact: "12312311231",
+            photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
+            tags: ["图书文具", "生活用品", "夹心糖"], //标签
+            readingtimes: 49, //阅读次数
+            comments: 5, //评论数量
+            favour: 20, //点赞数量
+            had_favour: 0, //点赞判断
+            favour_src: "/assets/images/icon/unfavour.png", //点赞图标
+        },
+        {
+            blogger_id: 2, //文章所属id
+            blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
+            blogger_name: "zhiming", //博主昵称
+            blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
+            lostthing_topic: "帅哥一只1a", //标题
+            lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
+            lostthing_class: "2", //发布类别（不需要可以不填充
+            lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
+            lostthing_space: "南海校区", //
+            lostthing_space_detail: "G253与G252之间的交界处",
+            lostthing_contact: "12312311231",
+            photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
+            tags: ["图书文具", "生活用品", "夹心糖"], //标签
+            readingtimes: 49, //阅读次数
+            comments: 5, //评论数量
+            favour: 20000, //点赞数量
+            had_favour: 0, //点赞判断
+            favour_src: "/assets/images/icon/unfavour.png", //点赞图标
+        },
+    ],
         //页面切换相关数据
-        page: 1,
-        ani1: '',
-        ani2: '',
-        ani3: ''
+        current_Page: 0,
     },
     jumpToSearch: function () {
         wx.navigateTo({
@@ -131,7 +291,7 @@ Page({
                     height: listHeight + 'px'
                 })
             })
-        }, 800)
+        }, 100)
         this.setData({
             navH: app.globalData.navHeight
         });
@@ -195,4 +355,11 @@ Page({
             })
         }, 800)
     },
+
+    clicktab: function(e){
+        var pag = e.currentTarget.dataset.current;
+        this.setData({
+            current_Page:pag
+        })
+    }
 })
