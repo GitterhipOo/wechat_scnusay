1ad4b90653d1e1146c2ca320ed41ff6665a2bd55
林木 <87061701+GitterhipOo@users.noreply.github.com>
2023-02-19 18:28:05 +0800

    用户上传后同步了头像

--- miniprogram-1/pages/lostthing/index.js
+++ miniprogram-1/pages/lostthing/index.js
@@ -13,6 +13,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    photocou:0,//用户上传图片的数量
     top_using_res: wx.getMenuButtonBoundingClientRect(),
     windowInfo: wx.getWindowInfo(),
     navH: 0,
@@ -84,106 +85,7 @@ Page({
         had_favour: 0, //点赞判断
         favour_src: "/assets/images/icon/unfavour.png", //点赞图标
       },
-      {
-        blogger_id: 6, //文章所属id
-        blogger_Openid: "ouctO4ypxLjQ_3t67gYI-urvPoQs",
-        blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
-        blogger_name: "zhiming", //博主昵称
-        blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
-        lostthing_topic: "巴拉拉一22222", //标题
-        lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
-        lostthing_class: "1", //发布类别（不需要可以不填充
-        lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
-        lostthing_space: "南海校区", //
-        lostthing_space_detail: "G253与G252之间的交界处",
-        lostthing_contact: "12312311231",
-        photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
-        readingtimes: 49, //阅读次数
-        comments: 5, //评论数量
-        favour: 20000, //点赞数量
-        had_favour: 0, //点赞判断
-        favour_src: "/assets/images/icon/unfavour.png", //点赞图标
-      },
-      {
-        blogger_id: 5, //文章所属id
-        blogger_Openid: "ouctO4ypxLjQ_3t67gYI-urvPoQs",
-        blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
-        blogger_name: "zhiming", //博主昵称
-        blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
-        lostthing_topic: "巴拉拉一22222", //标题
-        lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
-        lostthing_class: "1", //发布类别（不需要可以不填充
-        lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
-        lostthing_space: "南海校区", //
-        lostthing_space_detail: "G253与G252之间的交界处",
-        lostthing_contact: "12312311231",
-        photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
-        readingtimes: 49, //阅读次数
-        comments: 5, //评论数量
-        favour: 20000, //点赞数量
-        had_favour: 0, //点赞判断
-        favour_src: "/assets/images/icon/unfavour.png", //点赞图标
-      },
-      {
-        blogger_id: 4, //文章所属id
-        blogger_Openid: "ouctO4ypxLjQ_3t67gYI-urvPoQs",
-        blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
-        blogger_name: "zhiming", //博主昵称
-        blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
-        lostthing_topic: "巴拉拉一22222", //标题
-        lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
-        lostthing_class: "1", //发布类别（不需要可以不填充
-        lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
-        lostthing_space: "南海校区", //
-        lostthing_space_detail: "G253与G252之间的交界处",
-        lostthing_contact: "12312311231",
-        photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
-        readingtimes: 49, //阅读次数
-        comments: 5, //评论数量
-        favour: 20000, //点赞数量
-        had_favour: 0, //点赞判断
-        favour_src: "/assets/images/icon/unfavour.png", //点赞图标
-      },
-      {
-        blogger_id: 3, //文章所属id
-        blogger_Openid: "ouctO4ypxLjQ_3t67gYI-urvPoQs",
-        blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
-        blogger_name: "zhiming", //博主昵称
-        blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
-        lostthing_topic: "巴拉拉一22222", //标题
-        lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
-        lostthing_class: "1", //发布类别（不需要可以不填充
-        lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
-        lostthing_space: "南海校区", //
-        lostthing_space_detail: "G253与G252之间的交界处",
-        lostthing_contact: "12312311231",
-        photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
-        readingtimes: 49, //阅读次数
-        comments: 5, //评论数量
-        favour: 20000, //点赞数量
-        had_favour: 0, //点赞判断
-        favour_src: "/assets/images/icon/unfavour.png", //点赞图标
-      },
-      {
-        blogger_id: 1, //文章所属id
-        blogger_Openid: "ouctO4ypxLjQ_3t67gYI-urvPoQs",
-        blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
-        blogger_name: "zhiming", //博主昵称
-        blogger_time: "2022年9月20日", //发布时间的时间戳、这里需要修改
-        lostthing_topic: "巴拉拉一22222", //标题
-        lostthing_time: "2022年9月20日", //丢失时间的时间戳、这里需要修改
-        lostthing_class: "1", //发布类别（不需要可以不填充
-        lostthing_detail: "我在南海这里丢失了一块抹茶拿铁，你们可以帮我寻找一下遗失的红色精灵吗", //主要内容
-        lostthing_space: "南海校区", //
-        lostthing_space_detail: "G253与G252之间的交界处",
-        lostthing_contact: "12312311231",
-        photos: ["https://s1.328888.xyz/2022/08/29/CzMYU.png", "https://s1.328888.xyz/2022/08/29/CzgoR.png", "https://s1.328888.xyz/2022/08/29/Czf0B.png"], //放置于主要内容下方的图片
-        readingtimes: 49, //阅读次数
-        comments: 5, //评论数量
-        favour: 20000, //点赞数量
-        had_favour: 0, //点赞判断
-        favour_src: "/assets/images/icon/unfavour.png", //点赞图标
-      },
+
     ],
     //postowner为私人发布内容，根据时间排序
     postOwner: [
@@ -354,22 +256,7 @@ Page({
    */
   onLoad: function (options) {
     var _this = this
-    setTimeout(function () { //异步
-      var query = wx.createSelectorQuery();
-      query.selectAll('.list').boundingClientRect()
-      query.exec((res) => {
-        console.log(res)
-        var listHeight = res[0][0].height
-        console.log(res[0][0].height)
-        _this.setData({
-          heights: res[0],
-          height: listHeight + 40 + 'px'
-        })
-      })
-    }, 100)
-    this.setData({
-      navH: app.globalData.navHeight
-    });
+
     //onload的时候需要从服务器获取数据
     wx.request({
         url: 'https://www.scnusay.cc/lostdetail/lostdetailphoto/getdetail.php',
@@ -388,9 +275,8 @@ Page({
                   var newarray = {
                       blogger_id:res.data[i].id,
                       blogger_Openid: res.data[i].openid,
-                      blogger_avatar: "https://s1.328888.xyz/2022/08/02/OF8Ay.jpg", //头像
-                      blogger_name: "xhiming", //博主昵称
-                      //以上未返回
+                      blogger_avatar: res.data[i].imgurl, //头像
+                      blogger_name: res.data[i].name, //博主昵称
                       blogger_time: res.data[i].blogger_time, //发布时间的时间戳、这里需要修改
                       lostthing_topic: res.data[i].lostthing_topic, //标题
                       lostthing_time: res.data[i].lostthing_time, //丢失时间的时间戳、这里需要修改
@@ -405,13 +291,33 @@ Page({
                       favour: res.data[i].favour, //点赞数量
                       had_favour: 0, //点赞判断
                   }
+                  
+         
                   _this.setData({
                       post0: _this.data.post0.concat(newarray),
                       //将数组插入post0
                   })
               }
           }
+          
       })
+      
+          setTimeout(function () { //异步
+      var query = wx.createSelectorQuery();
+      query.selectAll('.list').boundingClientRect()
+      query.exec((res) => {
+        console.log(res)
+        var listHeight = res[0][0].height
+        console.log(res[0][0].height)
+        _this.setData({
+          heights: res[0],
+          height: listHeight + 40 + 'px'
+        })
+      })
+    }, 100)
+    this.setData({
+      navH: app.globalData.navHeight
+    });
   },
   logo: function (e) {
     //跳转去首页
