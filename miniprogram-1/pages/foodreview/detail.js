39b8060ba018a4d376337595b9e05c95637df611
akai34 <1579092193@qq.com>
2023-03-25 00:53:47 +0800

    加入了刷新（detail有点问题

--- miniprogram-1/pages/foodreview/detail.js
+++ miniprogram-1/pages/foodreview/detail.js
@@ -303,7 +303,21 @@ Page({
         console.log("now begin request");
 
         
-        wx.removeStorageSync('foodreviewsendPostValue')
     },
 
+    onPullDownRefresh: function () {
+        if (this.data.isRefreshing || this.data.isLoadingMoreData) {
+            return
+        }
+        this.setData({
+            isRefreshing: true,
+            hasMoreData: true
+        })
+        //wait for 1 second
+        setTimeout(() => {
+            wx.reLaunch({
+                url: 'detail',
+              })
+        }, 300)
+    },
 })
